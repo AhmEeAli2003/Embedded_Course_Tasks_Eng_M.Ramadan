37d0ab36f84080f1c0f752bbed8cf1e66df11084
AhmEeAli2003 <68246583+AhmEeAli2003@users.noreply.github.com>
2022-08-31 19:28:20 +0200

    Update task_5.c

--- task_5.c
+++ task_5.c
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <math.h>
 
-
-//double power(double base, double exponent);
-
 void main(void)
 {
 	double base = 0.0, exponent = 0.0;
@@ -18,41 +15,3 @@ void main(void)
 	
 	printf("%.2lf ^ %.2lf = %.2lf \n", base, exponent, pow(base, exponent));
 }
-
-/******* User define function *************
-//Logical Error 
-double power(double base, double exponent)
-{
-	double temp = base;
-	int i = 0;
-	
-	if (exponent == 0) 
-	{
-		return 1;
-	}
-	else if(exponent > 0)
-	{
-		if(exponent == 1) 
-		{
-			return base;
-		}
-		else
-		{
-			for ( i = 1; i < exponent; i++) 
-			{
-				base *= temp;
-			}
-			return base;
-		}
-	}
-	else if (exponent < 0)
-	{
-		exponent = abs(exponent);
-		for (i = 1; i < exponent; i++) 
-		{
-			base *= temp;
-		}
-		return (1.0/base);
-	}
-	else {}
-}*/
